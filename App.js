1c9ec86c7d3c7dd8503c46b7df2b74c1ceb5c310
김수민 <krlatnalsl01@gmail.com>
2023-06-15 15:05:33 +0900

    [modify] 코드정리 및 구현완료

--- App.js
+++ App.js
@@ -8,11 +8,15 @@ import {
   ScrollView,
   Dimensions,
   ActivityIndicator,
+  TouchableOpacity,
 } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
-import { useEffect, useState } from "react";
+import { Fontisto } from "@expo/vector-icons";
+import { useEffect, useState, useLayoutEffect } from "react";
 import * as Location from "expo-location";
+import moment from "moment";
+
 const SCREEN_WIDTH = Dimensions.get("window").width; //해당 기기의 위드를 가져옴
 const API_KEY = "9085b59875dd89f0d4bbe5779c2a300b";
 
@@ -20,11 +24,21 @@ export default function App() {
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
   const [city, setCity] = useState("Loading...");
+  const [date, setDate] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  const weatherIcon = {
+    Clouds: "cloudy",
+    Clear: "day-sunny",
+    Atmosphere: "cloudy-gusts",
+    Snow: "snow",
+    Rain: "rains",
+    Drizzle: "rain",
+    Thunderstorm: "lightning",
+  };
 
   const getWeather = async () => {
-    const permission = await Location.requestForegroundPermissionsAsync(); //위치 허용 요청
     const { granted } = await Location.requestForegroundPermissionsAsync(); //위치 허용 요청
-    // console.log("permission", permission);
 
     if (!granted) {
       //유저가 위치 허용 요청을 거절하면
@@ -34,27 +48,47 @@ export default function App() {
     const {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync({ accuracy: 6 }); //유저의 위치(위도와 경도)를 가져옴
-    // console.log();
+
     const location = await Location.reverseGeocodeAsync(
       //위의 받아온 좌표로 나라를 나타냄
       { latitude, longitude },
       { useGoogleMaps: false }
     );
     setCity(location[0].city);
-    // // console.log("location", location[0].city);
+    // console.log("location", location[0].city);
 
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alert&appid=${API_KEY}&units=metric`
     );
     const json = await response.json();
-    console.log(json);
     setDays(json.daily);
   };
 
-  // console.log(days);
-  useEffect(() => {
+  const changeDate = (i) => {
+    // console.log("days", days.length - 1);
+    // console.log("i", i);
+    days.length - 1 === i && setDate(true);
+    i === 0 && setDate(false);
+
+    if (index < 8 || index >= 0) {
+      if (date) {
+        setIndex((i) => i - 1);
+        return;
+      }
+      setIndex((i) => i + 1);
+      return;
+    }
+  };
+
+  useLayoutEffect(() => {
     getWeather();
   }, []);
+
+  // useEffect(() => {
+  //   console.log("index", index);
+  //   console.log(date);
+  //   console.log("------");
+  // }, [index, date]);
 
   return (
     <View style={styles.container}>
@@ -67,27 +101,59 @@ export default function App() {
           <Feather name="search" size={24} color="black" />
         </Pressable>
       </View>
-      <View style={styles.city}>
-        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Monday</Text>
-        <Text style={{ fontSize: 18, marginTop: 5 }}>04 September</Text>
-      </View>
+
       {ok ? (
         <>
           <ScrollView
             horizontal //수평
             pagingEnabled //스크롤을 완전히 넘겨야 넘겨지는 props
             showsHorizontalScrollIndicator={false} // pagingEnabled을 사용했을때 아래에 스크롤바의 유무
-            contentContainerStyle={styles.temperature} //일반 스타일은 안먹힘
+            disableScrollViewPanResponder={true}
           >
             {days.length > 0 ? (
               days.map((item, i) => (
-                <View key={i} style={styles.day}>
-                  <Text style={{ fontSize: 120, fontWeight: "bold" }}>
-                    {`${parseFloat(item.temp.day).toFixed(1)}º`}
-                  </Text>
-                  <Text style={{ fontSize: 20 }}>{item.weather[0].main}</Text>
-                  <Text>{item.weather[0].description}</Text>
-                </View>
+                <>
+                  <TouchableOpacity key={i} style={styles.day}>
+                    <View style={styles.city}>
+                      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+                        {moment().add(i, "d").format("dddd")}
+                      </Text>
+                      <Text style={{ fontSize: 18, marginTop: 5 }}>
+                        {moment().add(i, "d").format("DD MMMM")}
+                      </Text>
+                    </View>
+                    <Text
+                      style={{
+                        fontSize: 100,
+                        fontWeight: "bold",
+                        marginTop: 40,
+                        flex: 0.2,
+                      }}
+                    >
+                      {`${parseFloat(item.temp.day).toFixed(1)}º`}
+                    </Text>
+                    <View
+                      style={{
+                        justifyContent: "space-between",
+                        flexDirection: "row",
+                        paddingRight: 20,
+                        marginTop: 20,
+                      }}
+                    >
+                      <Text style={{ fontSize: 20 }}>
+                        {item.weather[0].main}
+                      </Text>
+                      <Fontisto
+                        name={weatherIcon[item.weather[0].main]}
+                        size={30}
+                        color="black"
+                      />
+                    </View>
+                    <Text style={{ flex: 0.5 }}>
+                      {item.weather[0].description}
+                    </Text>
+                  </TouchableOpacity>
+                </>
               ))
             ) : (
               <View style={styles.day}>
@@ -95,17 +161,6 @@ export default function App() {
               </View>
             )}
           </ScrollView>
-
-          <View style={styles.footer}>
-            <View style={{ justifyContent: "space-between" }}>
-              <Text style={{ fontSize: 18, fontWeight: "bold" }}>21</Text>
-              <Text style={{ fontSize: 18 }}>0% Precipitation</Text>
-            </View>
-            <View style={{ justifyContent: "space-between" }}>
-              <Text style={{ fontSize: 18 }}>8</Text>
-              <Text style={{ fontSize: 18, marginTop: 3 }}>3 km/h Wind</Text>
-            </View>
-          </View>
         </>
       ) : (
         <Text> 위치를 허용해 주세요. </Text>
@@ -123,34 +178,27 @@ const styles = StyleSheet.create({
     paddingLeft: 15,
   },
   top: {
-    flex: 0.5,
+    flex: 0.2,
     justifyContent: "space-between",
     alignItems: "flex-end",
     flexDirection: "row",
   },
   city: {
-    flex: 0.6,
+    flex: 0.4,
     // backgroundColor: "green",
     justifyContent: "center",
     borderBottomColor: "black",
     borderStyle: "solid",
     borderBottomWidth: 3,
   },
-  temperature: {
-    // backgroundColor: "orange",
+
+  day: {
+    // flex: 1,
+    width: SCREEN_WIDTH - 30,
+    justifyContent: "center",
     borderBottomColor: "black",
     borderStyle: "solid",
     borderBottomWidth: 3,
-  },
-  day: {
-    width: SCREEN_WIDTH - 30,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  footer: {
-    flex: 1,
-    // backgroundColor: "blue",
-    justifyContent: "center",
-    alignContent: "center",
+    // marginTop: -30,
   },
 });
